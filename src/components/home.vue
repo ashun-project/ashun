4c454b95624f2cc8af981956124f87dce0f7df8d
ashun-project <sexlookashun@gmail.com>
2018-04-22 17:26:56 +0800

    n

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,11 +1,50 @@
 <template>
     <div class="home">
         <div class="header">
-            <span class="lf"><img src="logo.png"></span>
-            <span class="rf">登入/注册</span>
+            <div class="cont">
+                <span class="lf"><img src="logo.png"></span>
+                <span class="rf">登入/注册</span>
+            </div>
         </div>
         <div class="banner">
             <img src="banner.png" alt="">
+        </div>
+        <div class="data-list">
+            <ul>
+                <li>
+                    <img src="" alt="">
+                    <p>this is title</p>
+                </li>
+                <li>
+                    <img src="" alt="">
+                    <p>this is title</p>
+                </li>
+                <li>
+                    <img src="" alt="">
+                    <p>this is title</p>
+                </li>
+                <li>
+                    <img src="" alt="">
+                    <p>this is title</p>
+                </li>
+                <li>
+                    <img src="" alt="">
+                    <p>this is title</p>
+                </li>
+                <li>
+                    <img src="" alt="">
+                    <p>this is title</p>
+                </li>
+                <li>
+                    <img src="" alt="">
+                    <p>this is title</p>
+                </li>
+                <li>
+                    <img src="" alt="">
+                    <p>this is title</p>
+                </li>
+            </ul>
+            <div class="clr"></div>
         </div>
         <ul>
             <li v-for="(item, idx) in list" :key="idx" @click="goDetail(item)">
@@ -34,3 +73,39 @@
         }
     }
 </script>
+
+<style scoped>
+    .header{
+        height: 50px;
+        line-height: 5 0px;
+        border: 1px solid #eee;
+    }
+    .header .cont{
+        width: 1200px;
+        margin: 0 auto;
+    }
+    .banner{
+        height: 300px;
+        background: #999;
+    }
+    .data-list{
+        width: 1200px;
+        margin: 20px auto 0;
+        overflow: hidden;
+    }
+    .data-list ul{
+        width: 1300px;
+    }
+    .data-list li{
+        float: left;
+        width: 292px;
+        margin-right: 10px;
+        margin-bottom: 10px;
+    }
+    .data-list li img{
+        float: left;
+        width: 100%;
+        height: 300px;
+        background: #eee;
+    }
+    </style>
