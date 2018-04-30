e8041f52910956e5bf6bfce4511a18764dcd3c9d
ashun <ashun@gmail.com>
2018-04-30 13:31:37 +0800

    删除数据

--- src/components/delete_repeat.vue
+++ src/components/delete_repeat.vue
@@ -9,22 +9,36 @@
 
             }
         },
+        methods: {
+            deleteData (id) {
+                this.$http.post('/api/deteleRepeat', {id: id}).then(response => {
+                    console.log(response, 789)
+                })
+            }
+        },
         created () {
             this.$http.post('/api/getAllList').then(response => {
                 let arr = [];
                 let repeat = [];
-                response.data.forEach( (item) => {
-                    let re = arr.some(value => value.title === item.title);
-                    if (re) {
-                        repeat.push(item) // createTime
-                        this.$http.post('/api/deteleRepeat', {id: item.createTime}).then(response => {
-                            console.log(response, 789)
-                        })
-                    } else {
-                        arr.push(item)
-                    }
-                })
+                let name = "妈妈的朋友"
+                // response.data.forEach( (item) => {
+                //     let re = arr.some(value => value.title === item.title);
+                //     if (re) {
+                //         repeat.push(item) // createTime
+                //         this.$http.post('/api/deteleRepeat', {id: item.createTime}).then(response => {
+                //             console.log(response, 789)
+                //         })
+                //     } else {
+                //         arr.push(item)
+                //     }
+                // })
+                
+                let fil = response.data.filter(item => item.title.indexOf(name) > -1);
+                console.log(fil)
             })
+            let a = ['1524799753']
+            let vm = this;
+            
         }
     }
 </script>
