d6c8518c2d616960635be481c6ec12c573f29625
ashun-project <sexlookashun@gmail.com>
2018-05-03 10:05:31 +0800

    add bottom

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,13 @@
             <img src="@/resource/img/banner.png" alt="">
         </div>
         <router-view/>
+        <div class="footer">
+            <div>
+                警告︰本網站只這合十八歲或以上人士觀看。內容可能令人反感；不可將本網站的內容派發、傳閱、出售、出租、交給或借予年齡未滿18歲的人士或將本網站內容向該人士出示、播放或放映。<br/>
+                LEGAL DISCLAIMER WARNING: THIS FORUM CONTAINS MATERIAL WHICH MAY OFFEND AND MAY NOT BE DISTRIBUTED, CIRCULATED, SOLD, HIRED, GIVEN, LENT,SHOWN, PLAYED OR PROJECTED TO A PERSON UNDER THE AGE OF 18 YEARS.<br/>
+                站点申明：我们立足于美利坚合众国，受北美法律保护,未满18岁或被误导来到这里，请立即离开！
+            </div>
+        </div>
     </div>
 </template>
 
@@ -136,7 +143,15 @@ export default {
         width: 1200px;
         margin: 0 auto;
     }
-
+    .footer {
+        width: 100%;
+        background: #333;
+        color: #ccc;
+        padding: 50px 0;
+        line-height:50px;
+        font-size:14px;
+        text-align: center;
+    }
 
     /* loadding */
     .loading{
