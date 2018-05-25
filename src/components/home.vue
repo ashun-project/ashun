e127a084782a1f3bdc56d51fd79af0a55a212f91
ashun <ashun@gmail.com>
2018-05-25 19:36:25 +0800

    title modify

--- src/components/home.vue
+++ src/components/home.vue
@@ -42,7 +42,7 @@
         },
         methods: {
             replaceWord (item) {
-                let rul = /【MP4\/.+】|\[MP4\/.+\]|【RV原创】|【精品RV原创】|【无码\/MP4\/.+】/gi;
+                let rul = /【MP4\/.+】|\[MP4\/.+\]|【RV原创】|【精品RV原创】|【无码\/MP4\/.+】|【杏吧】/gi;
                 return item.replace(rul, '')
             },
             goDetail (item) {
