8b883a192c87473d23a325b4cc769a15930e4496
ashun <ashun@gmail.com>
2018-05-31 13:55:14 +0800

    modify

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,17 @@
         <suspend></suspend>
         <div class="header" v-if="!$route.meta.notHeader">
             <div class="cont">
-                <div class="lf"><router-link to="/"><img src="@/resource/img/logo.png"></router-link></div>
+                <div class="lf">
+                    <router-link to="/"><img src="@/resource/img/logo.png"></router-link>
+                </div>
                 <div class="rf">
                     <span>登入</span>
                     <span class="interval">|</span>
                     <span>注册</span>
                     <span class="interval">|</span>
-                    <span>交流QQ<font color="red">3257905932</font></span>
+                    <span>交流QQ
+                        <font color="red">3257905932</font>
+                    </span>
                 </div>
             </div>
             <div class="menu">
@@ -31,9 +35,7 @@
         </div>
         <div class="footer" v-if="!$route.meta.notFooter">
             <div class="pc">
-                警告︰本網站只這合十八歲或以上人士觀看。內容可能令人反感；不可將本網站的內容派發、傳閱、出售、出租、交給或借予年齡未滿18歲的人士或將本網站內容向該人士出示、播放或放映。<br/>
-                LEGAL DISCLAIMER WARNING: THIS FORUM CONTAINS MATERIAL WHICH MAY OFFEND AND MAY NOT BE DISTRIBUTED, CIRCULATED, SOLD, HIRED, GIVEN, LENT,SHOWN, PLAYED OR PROJECTED TO A PERSON UNDER THE AGE OF 18 YEARS.<br/>
-                站点申明：我们立足于美利坚合众国，受北美法律保护,未满18岁或被误导来到这里，请立即离开！
+                警告︰本網站只這合十八歲或以上人士觀看。內容可能令人反感；不可將本網站的內容派發、傳閱、出售、出租、交給或借予年齡未滿18歲的人士或將本網站內容向該人士出示、播放或放映。<br/> LEGAL DISCLAIMER WARNING: THIS FORUM CONTAINS MATERIAL WHICH MAY OFFEND AND MAY NOT BE DISTRIBUTED, CIRCULATED, SOLD, HIRED, GIVEN, LENT,SHOWN, PLAYED OR PROJECTED TO A PERSON UNDER THE AGE OF 18 YEARS.<br/> 站点申明：我们立足于美利坚合众国，受北美法律保护,未满18岁或被误导来到这里，请立即离开！
             </div>
             <div class="h5">警告︰本網站只這合十八歲或以上人士觀看</div>
         </div>
@@ -51,155 +53,165 @@ export default {
         return {
             menu: [
                 {
-                    label: '在线区：', 
+                    label: '在线区：',
                     data: [
-                        {url: 'wumavideo', label: '无码性爱'},
-                        {url: 'sanjivideo', label: '三级在线'}
+                        { url: 'wumavideo', label: '无码性爱' },
+                        { url: 'sanjivideo', label: '三级在线' },
+                        { url: 'youmavideo', label: '有码性爱' },
+                        { url: 'dongmanvideo', label: '卡通动漫' },
+                        { url: 'oumeivideo', label: '欧美性爱' },
+                        { url: 'zipaivideo', label: '网友自拍' }
                     ]
                 },
                 {
-                    label: '下载区：', 
+                    label: '下载区：',
                     data: [
-                        {url: 'wuma', label: '经典无码'},
-                        {url: 'sanji', label: '三级影视'},
-                        {url: 'oumei', label: '欧美风情'},
+                        { url: 'wuma', label: '经典无码' },
+                        { url: 'sanji', label: '三级影视' },
+                        { url: 'oumei', label: '欧美风情' },
                         // {url: 'youma', label: '有码性爱'},
-                        {url: 'dongman', label: '卡通动漫'},
-                    // {url: 'tupian', label: '图片专区'},
-                    // {url: 'xiaoshou', label: '小说专区'}]}
+                        { url: 'dongman', label: '卡通动漫' },
+                        // {url: 'tupian', label: '图片专区'},
+                        // {url: 'xiaoshou', label: '小说专区'}]}
                     ]
                 }
             ]
-        }    
+        }
     }
 }
 </script>
 
 <style>
-    @import "./resource/css/style.css";
-    .header{
-        width: 100%;
-        background: #fff;
+@import './resource/css/style.css';
+.header {
+    width: 100%;
+    background: #fff;
+}
+.header .cont {
+    max-width: 1200px;
+    width: 100%;
+    height: 40px;
+    line-height: 40px;
+    margin: 0 auto;
+}
+.header .cont .lf {
+    color: #666;
+    font-size: 16px;
+}
+.header .cont .lf img {
+    height: 30px;
+    float: left;
+    margin-top: 5px;
+}
+.header .cont .rf {
+    font-size: 16px;
+    color: #666;
+}
+.header .cont .rf span {
+    display: inline-block;
+}
+.header .cont .rf span.interval {
+    padding: 0 5px;
+    color: #ccc;
+}
+.header .menu {
+    background: #f9d8dc;
+    padding: 5px 0;
+}
+.header .menu dl {
+    max-width: 1200px;
+    width: 100%;
+    margin: 0 auto;
+    line-height: 30px;
+    font-size: 16px;
+    padding: 2px 0 2px 70px;
+    position: relative;
+    display: flex;
+    align-items: center;
+    flex-wrap: wrap;
+}
+.header .menu dl dt {
+    color: rgb(121, 33, 33);
+    margin-right: 5px;
+    font-weight: 600;
+    position: absolute;
+    left: 2px;
+    top: 2px;
+}
+.header .menu dd {
+    float: left;
+    height: 30px;
+    line-height: 30px;
+    transition: all 1s;
+    width: 90px;
+    max-width: 100px;
+}
+.header .menu dd a {
+    display: block;
+    width: 100%;
+    height: 100%;
+    padding: 0 10px;
+    color: #576700;
+    text-align: center;
+}
+.header .menu dd.active a {
+    border-radius: 3px;
+    background: #cae243;
+    color: #fff;
+}
+.header .menu dd.active a:hover {
+    color: #fff;
+}
+.header .menu dd a:hover {
+    color: #cae243;
+}
+.banner {
+    max-height: 300px;
+    width: 100%;
+    max-width: 1200px;
+    margin: 0 auto;
+}
+.banner img {
+    width: 100%;
+}
+.body-content .ashun {
+    position: fixed;
+    top: 50%;
+    left: 0%;
+    transform: translateY(-50%);
+    z-index: -1;
+}
+.footer {
+    width: 100%;
+    background: #333;
+    color: #ccc;
+    padding: 50px 0;
+    line-height: 50px;
+    font-size: 14px;
+    text-align: center;
+}
+.footer .h5 {
+    display: none;
+}
+@media screen and (max-width: 600px) {
+    .header .cont .rf {
+        padding-right: 5px;
     }
-    .header .cont{
-        max-width: 1200px;   
-        width: 100%;
-        height: 40px;
-        line-height: 40px;
-        margin: 0 auto;
+    .header .menu dl {
+        padding-left: 60px;
+        font-size: 14px;
     }
-    .header .cont .lf{
-        color: #666;
-        font-size: 16px;
-    }
-    .header .cont .lf img{
-        height: 30px;
-        float: left;
-        margin-top: 5px;
-    }
-    .header .cont .rf{
-        font-size: 16px;
-        color: #666;
-    }
-    .header .cont .rf span{
-        display: inline-block;
-    }
-    .header .cont .rf span.interval{
-        padding: 0 5px;
-        color: #ccc;
-    }
-    .header .menu{
-        background:#f9d8dc;
-        padding: 5px 0;
-    }
-    .header .menu dl{
-        max-width: 1200px;
-        width: 100%;
-        margin: 0 auto;
-        height: 34px;
-        line-height: 30px;
-        font-size: 16px;
-        padding: 2px 0 2px 70px;
-        position: relative;
-    }
-    .header .menu dl dt{
-        color: rgb(121, 33, 33);
-        margin-right: 5px;
-        font-weight: 600;
-        float: left;
-        position: absolute;
-        left: 2px;
-        top: 2px;
-    }
-    .header .menu dd{
-        float: left;
-        height: 30px;
-        line-height: 30px;
-        transition: all 1s;
-    }
-    .header .menu dd a{
-        display: block;
-        width: 100%;
-        height: 100%;
-        padding: 0 10px;
-        color: #576700;
-        text-align: center;
-    }
-    .header .menu dd.active a{
-        border-radius: 3px;
-        background: #cae243;
-        color: #fff;
-    }
-    .header .menu dd.active a:hover{
-        color: #fff;
-    }
-    .header .menu dd a:hover{
-        color: #cae243;
-    }
-    .banner{
-        max-height: 300px;
-        width: 100%;
-        max-width: 1200px;
-        margin: 0 auto;
-    }
-    .banner img{
-        width: 100%;
-    }
-    .body-content .ashun{
-        position: fixed;
-        top: 50%;
-        left: 0%;
-        transform: translateY(-50%);
-        z-index: -1;
+    .header .menu dl dd {
+        width: 33%;
     }
     .footer {
-        width: 100%;
-        background: #333;
-        color: #ccc;
-        padding: 50px 0;
-        line-height:50px;
-        font-size:14px;
-        text-align: center;
+        padding: 10px 0;
     }
-    .footer .h5{
+    .footer .h5 {
+        display: block;
+    }
+    .footer .pc {
         display: none;
     }
-    @media screen and (max-width: 600px) {
-        .header .cont .rf{
-            padding-right: 5px;
-        }
-        .header .menu dl{
-            padding-left: 60px;
-            font-size: 14px;
-        }
-        .footer{
-            padding: 10px 0;
-        }
-        .footer .h5{
-            display: block;
-        }
-        .footer .pc{
-            display: none;
-        }
-    }
+}
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -10,65 +10,84 @@ import test from '@/components/test'
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      redirect: '/wumavideo'
-    },
-    {
-      path: '/wumavideo',
-      name: 'wumavideo',
-      component: videoList
-    },
-    {
-      path: '/sanjivideo',
-      name: 'sanjivideo',
-      component: videoList
-    },
-    {
-      path: '/videoDetail/:title/:id',
-      name: 'videoDetail',
-      component: videoDetail
-    },
-    {
-      path: '/sanji',
-      name: 'sanji',
-      component: home
-    },
-    {
-      path: '/wuma',
-      name: 'wuma',
-      component: home
-    },
-    {
-      path: '/oumei',
-      name: 'oumei',
-      component: home
-    },
-    {
-      path: '/dongman',
-      name: 'dongman',
-      component: home
-    },
-    {
-      path: '/detail/:title/:id',
-      name: 'detail',
-      component: detail
-    },
-    {
-      path: '/deleteRepeat/:id/:age/:name/:all',
-      name: 'deleteRepeat',
-      component: deleteRepeat,
-      meta: {
-        notFooter: true,
-        notHeader: true
-      }
-    },
-    {
-      path: '/test',
-      name: 'test',
-      component: test
-    }
-  ]
+    routes: [{
+            path: '/',
+            name: 'home',
+            redirect: '/wumavideo'
+        },
+        {
+            path: '/wumavideo',
+            name: 'wumavideo',
+            component: videoList
+        },
+        {
+            path: '/sanjivideo',
+            name: 'sanjivideo',
+            component: videoList
+        },
+        {
+            path: '/youmavideo',
+            name: 'youmavideo',
+            component: videoList
+        },
+        {
+            path: '/oumeivideo',
+            name: 'oumeivideo',
+            component: videoList
+        },
+        {
+            path: '/dongmanvideo',
+            name: 'dongmanvideo',
+            component: videoList
+        },
+        {
+            path: '/zipaivideo',
+            name: 'zipaivideo',
+            component: videoList
+        },
+        {
+            path: '/videoDetail/:title/:id',
+            name: 'videoDetail',
+            component: videoDetail
+        },
+        {
+            path: '/sanji',
+            name: 'sanji',
+            component: home
+        },
+        {
+            path: '/wuma',
+            name: 'wuma',
+            component: home
+        },
+        {
+            path: '/oumei',
+            name: 'oumei',
+            component: home
+        },
+        {
+            path: '/dongman',
+            name: 'dongman',
+            component: home
+        },
+        {
+            path: '/detail/:title/:id',
+            name: 'detail',
+            component: detail
+        },
+        {
+            path: '/deleteRepeat/:id/:age/:name/:all',
+            name: 'deleteRepeat',
+            component: deleteRepeat,
+            meta: {
+                notFooter: true,
+                notHeader: true
+            }
+        },
+        {
+            path: '/test',
+            name: 'test',
+            component: test
+        }
+    ]
 })
