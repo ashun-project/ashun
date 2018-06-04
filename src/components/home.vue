45716fc241ba8eb66857a07a5e569ba4f15f0220
ashun <ashun@gmail.com>
2018-06-04 12:02:08 +0800

    nothing

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="home">
-        
+
         <div class="data-list">
             <ul>
                 <li v-for="(item, idx) in list" :key="idx" @click="goDetail(item)">
@@ -23,52 +23,52 @@
     </div>
 </template>
 <script>
-    import vPagination from "@/components/common/page"
-    export default {
-        data () {
-            return {
-                list: [],
-                total: 0,
-                current: 1
-            }
-        },
-        components:{
-            vPagination
-        },
-        watch: {
-            $route (n, o) {
-                console.log(n, '===============')
-                this.getList(1)
-            }
-        },
-        methods: {
-            replaceWord (item) {
-                let rul = /【mp4下载】|\[mp4下载\]|【MP4\/.+】|\[MP4\/.+\]|【RV原创】|【精品RV原创】|【无码\/MP4\/.+】|【杏吧】/gi;
-                return item.replace(rul, '')
-            },
-            goDetail (item) {
-                window.open('#/detail/' + this.$route.name + '/' + item.id, '_blank');
-            },
-            pagechange (num) {
-                this.getList (num)
-            },
-            getList (current) {
-                console.log(this.$route.name, '==========')
-                this.$http.post('/api/getList', {current: current, title: this.$route.name}).then(response => {
-                    console.log(response, 787454121)
-                    response.data.list.forEach(item => {
-                        if (item.img.indexOf('https:') > -1) {
-                            item.img = item.img.replace('https:', '');
-                        }
-                    })
-                    this.list = response.data.list;
-                    this.total = response.data.total;
-                })
-            }
-        },
-        created () {
+import vPagination from "@/components/common/page"
+export default {
+    data () {
+        return {
+            list: [],
+            total: 0,
+            current: 1
+        }
+    },
+    components: {
+        vPagination
+    },
+    watch: {
+        $route (n, o) {
+            console.log(n, '===============')
             this.getList(1)
         }
+    },
+    methods: {
+        replaceWord (item) {
+            let rul = /【mp4下载】|\[mp4下载\]|【MP4\/.+】|\[MP4\/.+\]|【RV原创】|【精品RV原创】|【无码\/MP4\/.+】|【杏吧】/gi;
+            return item.replace(rul, '')
+        },
+        goDetail (item) {
+            window.open('#/detail/' + this.$route.name + '/' + item.id, '_blank');
+        },
+        pagechange (num) {
+            this.getList(num)
+        },
+        getList (current) {
+            console.log(this.$route.name, '==========')
+            this.$http.post('/api/getList', { current: current, title: this.$route.name }).then(response => {
+                console.log(response, 787454121)
+                response.data.list.forEach(item => {
+                    if (item.img.indexOf('https:') > -1) {
+                        item.img = item.img.replace('https:', '');
+                    }
+                })
+                this.list = response.data.list;
+                this.total = response.data.total;
+            })
+        }
+    },
+    created () {
+        this.getList(1)
     }
+}
 </script>
 
